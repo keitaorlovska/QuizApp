559932626ff4ec58dd93d3f7182b3d362668abb9
keitaorlovska <100114780+keitaorlovska@users.noreply.github.com>
2022-03-28 08:32:50 +0300

    pielikam about

--- server.py
+++ server.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 @app.route('/',methods = ['POST', 'GET'])
 def root():
     return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
     
 #Pārbaudes lapa, lai saprastu, ka kods vispār strādā
 @app.route('/health')
